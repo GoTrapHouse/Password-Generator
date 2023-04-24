9d9b1f34d92fbeb89a7d06dd4b01d27d3ba5bc8c
TrapHouse <130294818+GoTrapHouse@users.noreply.github.com>
2023-04-24 20:08:22 +0500

    Add files via upload

---
+++ ConsoleApp1/PasswordGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class PasswordGenerator
+    {
+        private static readonly Random random = new Random();
+
+        public static string GeneratePassword(int length, bool includeDigits, bool includeSymbols)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            const string digits = "0123456789";
+            const string symbols = "!@#$%^&*()_-+=[]{}\\|;:'\",.<>/?";
+
+            var passwordChars = new char[length];
+            int charIndex = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                string charPool = chars;
+
+                if (includeDigits && i % 3 == 0)
+                {
+                    charPool += digits;
+                }
+
+                if (includeSymbols && i % 5 == 0)
+                {
+                    charPool += symbols;
+                }
+
+                passwordChars[charIndex++] = charPool[random.Next(charPool.Length)];
+            }
+
+            string password = new string(passwordChars);
+            string hiddenPassword = new string('*', length);
+
+            Console.WriteLine("Вы успешно сгенерировали пароль:");
+            Console.WriteLine($"Пароль: {hiddenPassword}");
+            Console.WriteLine($"Длина пароля: {length}");
+            Console.WriteLine($"Содержит цифры: {(includeDigits ? "Да" : "Нет")}");
+            Console.WriteLine($"Содержит символы: {(includeSymbols ? "Да" : "Нет")}");
+
+            return password;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace ConsoleApp1;
+class Program
+{
+
+    public static void Main()
+    {
+        Console.WriteLine("Вы запустили генератор пароля\nBy TrapHouse");
+
+
+        int length;
+        while (true)
+        {
+            Console.Write("Введите длину пароля: ");
+            if (int.TryParse(Console.ReadLine(), out length))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+            }
+        }
+
+        Console.Write("Включить цифры? (да/нет): ");
+        bool includeDigits = Console.ReadLine().ToLower() == "да";
+
+        Console.Write("Включить символы? (да/нет): ");
+        bool includeSymbols = Console.ReadLine().ToLower() == "да";
+
+        string password = PasswordGenerator.GeneratePassword(length, includeDigits, includeSymbols);
+
+        Console.Write("Показать пароль? (да/нет): ");
+        bool showPassword = Console.ReadLine().ToLower() == "да";
+
+        if (showPassword)
+        {
+            Console.WriteLine($"Пароль: {password}");
+        }
+        
+        Console.WriteLine("Благодарю вас, за то что воспользовались моим генератором)");
+    }
+}
